639b73ebf6ad85e4e1f4197d11a5b8bab4c99dfc
tiger <897307189@qq.com>
2020-07-29 15:00:46 +0800

    第一课：打印和变量

--- basic/Hello_world.py
+++ basic/Hello_world.py
@@ -1,1 +1,18 @@
-print("hello world")
+#print函数为python显示函数,python3为关键字print加括号打印内容
+#打印字符串（字符串：就是一系列字符。在python中用引号括起来的都是字符串，其中引号包括双引号和单引号）
+print("Hello Python World")
+#输出：Hello Python World
+
+#打印数字
+print(123)
+#输出：123
+
+#变量，变量是指在程序运行时其值可以改变的量,变量的功能就是存储数据，
+# Pyhton是弱类型语言，所以不必像其他强类型语言那样声明变量的时候指定变量的类型；
+message = "我可以是字符串类型的内容"
+print(message)
+#输出：我可以是字符串类型的内容
+
+message = 123.34
+print(message)
+#输出：123.34
